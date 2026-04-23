0a558e040bbd38c30b4a040775623f4091aa500c
Jeaninny Teixeira <jeaninny.teixeira@gmail.com>
2026-04-23 12:49:31 -0300

    🔨 Refactor Code:  atualiza App.tsx com rotas completas e ToastContainer e configura fontes no index.css

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
 // Importações das Páginas Estáticas 
-import Home from "./pages/Home";
+
 import Sobre from "./pages/Sobre";
 
 // Importações de Usuário e Segurança
@@ -23,30 +23,40 @@ import FormCategoria from './components/categoria/formcategoria/FormCategoria'
 import DeletarCategoria from './components/categoria/deletarcategoria/DeletarCategoria'
 import ListarExercicios from './components/exercicios/listarexercicios/ListarExercicios'
 import FormExercicio from './components/exercicios/formexercicios/FormExercicios'
+import Projeto from './pages/Projeto';
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <AuthProvider>
-      <ToastContainer />
       <BrowserRouter>
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={true}
+          closeOnClick={true}
+          pauseOnHover={false}
+          draggable={false}
+          theme="dark"
+        />        
         <div className="min-h-screen bg-[#080D1A] text-[#F0F4FF] flex flex-col font-sans relative overflow-hidden">
-          
-          <div className="absolute inset-0 z-0 opacity-10" 
-               style={{ backgroundImage: `radial-gradient(#8B9DC3 1px, transparent 1px)`, backgroundSize: '24px 24px' }}>
+
+          <div className="absolute inset-0 z-0 opacity-10"
+            style={{ backgroundImage: `radial-gradient(#8B9DC3 1px, transparent 1px)`, backgroundSize: '24px 24px' }}>
           </div>
 
           <div className="absolute top-[20%] left-[-5%] w-100 h-100 rounded-full bg-[#F59E0B]/10 blur-[120px] z-0"></div>
           <div className="absolute bottom-[10%] right-[0%] w-75 h-75 rounded-full bg-[#38BDF8]/5 blur-[100px] z-0"></div>
 
           <Navbar />
-          
+
           <main className="flex-1 flex flex-col z-10 relative">
             <Routes>
               {/* Páginas Estáticas */}
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
               <Route path="/sobre" element={<Sobre />} />
-              <Route path="/projeto" element={<div className="flex-1" />} />
+              <Route path="/projeto" element={<Projeto />} />
 
               {/* Rotas de Autenticação */}
               <Route path="/login" element={<Login />} />
@@ -68,7 +78,7 @@ export default function App() {
               <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </main>
-          
+
           <Footer />
         </div>
       </BrowserRouter>
